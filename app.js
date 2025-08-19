11d47fbce7ef69832ae9b38835c264fa73b40b8f
Balint Mark Photography <balintmark.photography@Tamass-MacBook-Pro-2.local>
2025-08-19 18:51:11 +0300

    fix: Implement new endpoint

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
     const express = require('express');
     const app = express();
-
-    app.get('/', (req, res) => {
-      res.send('Hello, CI/CD World!');
-    });
+        app.get('/new', (req, res) => {
+          res.send('Hello, New Endpoint!');
+        });        
 
     module.exports = app;
